85a6a03736057cb7479087770a79013f90cc0361
anavarreteuk <a.navarrete.uk@gmail.com>
2018-09-16 19:42:45 +0100

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    map {|key, value| args.include?(value) ? key : nil }.compact
   end
 end
+=begin
+def keys_of(*arguments)
+    keys = []
+    self.each do |key, value|
+      if arguments.include?(value)
+        keys << key
+      end
+    end
+    keys
+  end
+end=
